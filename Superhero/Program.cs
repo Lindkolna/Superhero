7e0f7c3b665704cc0ce27782977e9480516d87db
Lindkolna <veestules.lindai@gmail.com>
2022-02-24 21:11:20 +0200

    SuperHero features

--- Superhero/Program.cs
+++ Superhero/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+String name = ("Fluffy");
+int age = 30;
+string HeroPower1, HeroPower2, HeroPower3;
+HeroPower1 = "can freeze you with a look";
+HeroPower2 = "can make fluffy clouds appear";
+HeroPower3 = "can make you tell only truth";
+
+Console.WriteLine(".................");
+Console.WriteLine($"Hero: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Powers: {HeroPower1}, {HeroPower2}, {HeroPower3}");
